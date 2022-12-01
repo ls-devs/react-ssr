cee9b026c45b4bb5627f82bbff63a51c0e813fbd
ls-devs <58638832+ls-devs@users.noreply.github.com>
2022-12-01 17:27:26 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Home from "./Home";
+
+function App() {
+  return <Home name="SSR" />;
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home(props) {
+  return <h1>Yo le {props.name}</h1>;
+}
+
+export default Home;
